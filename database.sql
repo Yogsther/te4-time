8f1d09579b2f40aeb1128de8791e0fab73a3a003
Olle Kaiser <yogsther@gmail.com>
2019-09-08 22:31:37 +0200

    Updated SQL

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Värd:                         127.0.0.1
--- Serverversion:                5.7.27-0ubuntu0.18.04.1 - (Ubuntu)
+-- Serverversion:                5.7.26-0ubuntu0.18.04.1 - (Ubuntu)
 -- Server-OS:                    Linux
--- HeidiSQL Version:             10.2.0.5599
+-- HeidiSQL Version:             10.1.0.5464
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -13,20 +13,19 @@
 
 
 -- Dumpar databasstruktur för te4
-CREATE DATABASE IF NOT EXISTS `te4` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+CREATE DATABASE IF NOT EXISTS `te4` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `te4`;
 
 -- Dumpar struktur för tabell te4.cards
 CREATE TABLE IF NOT EXISTS `cards` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` int(11) NOT NULL,
-  `rfid` text COLLATE utf8_bin NOT NULL,
+  `serial` text COLLATE utf8_bin NOT NULL,
   `active` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- Dataexport var bortvalt.
-
 -- Dumpar struktur för tabell te4.checks
 CREATE TABLE IF NOT EXISTS `checks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -37,18 +36,31 @@ CREATE TABLE IF NOT EXISTS `checks` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- Dataexport var bortvalt.
+-- Dumpar struktur för tabell te4.tokens
+CREATE TABLE IF NOT EXISTS `tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` int(11) DEFAULT NULL,
+  `token` text,
+  `created` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
+-- Dataexport var bortvalt.
 -- Dumpar struktur för tabell te4.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` text COLLATE utf8_bin NOT NULL,
   `last_name` text COLLATE utf8_bin NOT NULL,
+  `created` bigint(20) NOT NULL,
+  `barcode` text COLLATE utf8_bin NOT NULL,
+  `avatar` text COLLATE utf8_bin NOT NULL,
   `email` text COLLATE utf8_bin NOT NULL,
+  `access_token` text COLLATE utf8_bin,
+  `title` text COLLATE utf8_bin,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- Dataexport var bortvalt.
-
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
